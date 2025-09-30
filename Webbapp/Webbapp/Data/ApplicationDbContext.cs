bae39aaf8f93144a2719e578439a7c8308f2bc6a
BobthebuilderORIGINAL <b.hadjiyankov2021@pmg-pernik.com>
2025-09-30 11:55:15 +0300

    add project

---
+++ Webbapp/Webbapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using Webbapp.Data.Entities;
+
+namespace Webbapp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+          
+
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Purchase> Puchases { get; set; }
+    }
+}
---
+++ Webbapp/Webbapp/Data/Entities/Purchase.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Webbapp.Data.Entities
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        [ForeignKey("Car")]
+        [Required]
+        public int CarId { get; set; }
+        public Car Car { get; set; } = null!;
+
+        [ForeignKey("Client")]
+        [Required]
+
+        public int ClientId { get; set; }
+        public Client Client { get; set; } = null!;
+        [Required]
+
+        public DateTime PurchaseDate { get; set; }
+    }
+}
